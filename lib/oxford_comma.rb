99c91714459786a7cb349b7a6aaf57496144f447
Jennifer Prince <jennifergraceprince@gmail.com>
2019-10-07 04:37:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
-
+feelings = ["hungry, tired"]
+def oxford_comma(feelings)
+  if feelings.length == 2
+  feelings[-2] << " and "
+  feelings.join
+  elsif feelings.length == 1
+  	feelings.join
+  elsif feelings.length > 2
+    feelings[-1].prepend "and "
+	feelings.join(", ")
+  end
 end
